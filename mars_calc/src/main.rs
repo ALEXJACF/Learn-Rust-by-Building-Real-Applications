683bcdf05be072c56d357c84f2824c1461b1a221
Joao Fonseca <j90fonseca@gmail.com>
2025-07-30 11:34:01 +0100

    halfway done

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,16 +1,37 @@
 use std::io;
 
+const EARTH_GRAVITY: f32 = 9.81;
+const MARS_GRAVITY: f32 = 3.711;
+
+// Macro example in Rust to calculate weight on Mars.
+macro_rules! calculate_weight_on_mars {
+    ($weight:expr) => {
+        ($weight / EARTH_GRAVITY) * MARS_GRAVITY
+    };
+}
+
 fn main() {
     println!("Enter your weight (kg): ");
+    // Mutable string to hold user input.
     let mut input = String::new();
 
+    // If result was not ok, unwrap will terminate the program.
+    // If result was ok, unwrap will return the value.
     io::stdin().read_line(&mut input).unwrap();
 
+    //Remove whitespace and convert to f32.
     let weight: f32 = input.trim().parse().unwrap();
     let mars_weight = calculate_weight_on_mars(weight);
-    println!("Weight on Mars: {}kg", mars_weight);
+
+    // Needs mut keyword to modify the variable (Kg to grams).
+    //mars_weight = mars_weight * 1000.0;  // ERROR: cannot assign twice to immutable variable `mars_weight`
+
+    // Floating-point number to 2 decimal places.
+    println!("Function: Weight on Mars: {{{:.2} kg}}", mars_weight);
+
+    println!("Macro: Weight on Mars: {{{:.2} kg}}", calculate_weight_on_mars!(weight));
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
-    (weight / 9.81) * 3.711
+    (weight / EARTH_GRAVITY) * MARS_GRAVITY
 }
--- memory_management/src/main.rs
+++ memory_management/src/main.rs
@@ -1,3 +1,5 @@
+// Debugged with codelldb vscode extension
+
 fn main() {
     let a = 2;
     let result = stack_only(a);
@@ -5,12 +7,24 @@ fn main() {
 }
 
 fn stack_only(b: i32) -> i32 {
+    //breakpoint 
+    // b = 2
+    // c = 32766
     let c = 3;
+    //breakpoint 
+    // b = 2
+    // c = 3
     return b + c + stack_and_heap();
 }
 
 fn stack_and_heap() -> i32 {
+    //breakpoint 
+    // d = 0
+    // e = <null>
     let d = 5;
     let e = Box::new(7);
+    //breakpoint 
+    // d = 5
+    // *e = 7
     return d + *e;
 }
